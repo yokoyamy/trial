81a226ac6f6f10a1a42cbbdf0114646981ae8b12
yokoyamy <yokoyamy@jacic.or.jp>
2026-08-26 17:23:37 +0900

    JACICから同期: .poc/draft/アンケートアプリ/index.php

--- .poc/draft/アンケートアプリ/index.php
+++ .poc/draft/アンケートアプリ/index.php
@@ -1,25 +1,6 @@
 <?php
 declare(strict_types=1);
-error_reporting(E_ALL);
-ini_set('display_errors', '0');
-ini_set('display_startup_errors', '0');
-ini_set('log_errors', '1');
 
-/**
- * ---------------------------------------------------------
- * CORS / Preflight
- * ---------------------------------------------------------
- */
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: null');
-    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Accept, X-CSRF-Token');
-    header('Access-Control-Allow-Credentials: true');
-
-    http_response_code(204);
-    exit;
-}
 
 
 /**
